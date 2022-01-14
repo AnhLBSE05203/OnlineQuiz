5c8df6ace1d668919db040d36f86c80ed73910c5
phongbt2 <phongbt2@vnext.com.vn>
2022-01-14 15:21:59 +0700

    add link login and forget password admin

--- src/main/java/com/fpt/OnlineQuiz/controller/AdminController.java
+++ src/main/java/com/fpt/OnlineQuiz/controller/AdminController.java
@@ -2,7 +2,6 @@ package com.fpt.OnlineQuiz.controller;
 
 import com.fpt.OnlineQuiz.service.AccountService;
 import com.fpt.OnlineQuiz.service.MailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +11,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class AdminController {
 
-    @Autowired
-    private AccountService accountService;
-    @Autowired
-    private MailService mailService;
+    private final AccountService accountService;
+    private final MailService mailService;
+
+    public AdminController(AccountService accountService, MailService mailService) {
+        this.accountService = accountService;
+        this.mailService = mailService;
+    }
 
     @GetMapping("/login")
     String loginPage(Model model) {
-        return "login_page";
+        return "admin_login_page";
+    }
+
+    @GetMapping("/forget_pass")
+    String forgetPass() {
+        return "admin_forget_password";
     }
 
 
