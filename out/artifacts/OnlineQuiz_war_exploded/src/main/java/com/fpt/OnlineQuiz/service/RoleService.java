440394a82c1dbe542d3164454d4f6bef78d54532
Bao Anh Luu <anhlbse05203@fpt.edu.vn>
2022-02-08 10:56:02 +0700

    merge

--- out/artifacts/OnlineQuiz_war_exploded/src/main/java/com/fpt/OnlineQuiz/service/RoleService.java
+++
@@ -1,9 +0,0 @@
-package com.fpt.OnlineQuiz.service;
-
-import com.fpt.OnlineQuiz.model.Role;
-
-import java.util.List;
-
-public interface RoleService {
-    List<Role> findAll();
-}
--- out/artifacts/OnlineQuiz_war_exploded/src/main/java/com/fpt/OnlineQuiz/utils/Constants.java
+++
@@ -1,4 +0,0 @@
-package com.fpt.OnlineQuiz.utils;
-
-public class Constants {
-}
