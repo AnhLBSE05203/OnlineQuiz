a21ad225a20bd62c2df756644c681b1a22573b6f
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-02-17 21:27:24 +0700

    code backend question services

---
+++ src/main/java/com/fpt/OnlineQuiz/dao/QuestionRepository.java
@@ -0,0 +1,35 @@
+package com.fpt.OnlineQuiz.dao;
+
+import com.fpt.OnlineQuiz.model.Course;
+import com.fpt.OnlineQuiz.model.Question;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.Collection;
+import java.util.List;
+
+@Repository
+@Transactional
+public class QuestionRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Question> getQuestionsBySubjectId(int subject_id){
+        try {
+            //TODO Fix SQL
+            StringBuilder sb = new StringBuilder();
+            sb.append("select s.question from Subject s where s.id =:id");
+            String sql = sb.toString();
+            Query query = em.createQuery(sql, Question.class);
+            query.setParameter("id", subject_id);
+            query.setMaxResults(3);
+            return (List<Question>) query.getResultList();
+        }catch (NoResultException e){
+            return null;
+        }
+    }
+}
