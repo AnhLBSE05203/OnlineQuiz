f8fb231cd1252aa37b38f7f6e1d9b36d486fa00e
Bao Anh Luu <anhlbse05203@fpt.edu.vn>
2022-02-20 19:05:41 +0700

    fix mobile menu bug -move updateMobileMenu to user_header_footer.js ajax

--- src/main/resources/static/js/user_header_footer.js
+++ src/main/resources/static/js/user_header_footer.js
@@ -6,12 +6,26 @@ $(document).ready(function(){
                 contentType: "application/json; charset=utf-8",
                 dataType: "json",
                 success: function (data){
-                principal = data;
+                    principal = data;
                     if(principal != ""){
-                        $("#login-button").attr("href", "account/logout");
+                        $("#login-button").attr("href", "/account/logout");
                         $("#login-button").html("Logout");
                         $("#register-button").parent().css('visibility','hidden');
-                    }
-                }
+                    };
+                    updateMobileMenu();
+                },
+                error: function(){
+                    updateMobileMenu();
+                },
             });
 });
+function updateMobileMenu(){
+    var menu = $('ul#navigation');
+    if(menu.length){
+        menu.slicknav({
+        prependTo: ".mobile_menu",
+        closedSymbol: '+',
+        openedSymbol:'-'
+        });
+    };
+};
