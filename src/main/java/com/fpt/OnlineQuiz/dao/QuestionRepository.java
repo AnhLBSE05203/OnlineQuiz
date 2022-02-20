723bf010d115827777462f48c3b0d17095c2f168
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-02-20 22:46:28 +0700

    update QuestionRepository.java

--- src/main/java/com/fpt/OnlineQuiz/dao/QuestionRepository.java
+++ src/main/java/com/fpt/OnlineQuiz/dao/QuestionRepository.java
@@ -9,6 +9,7 @@ import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.transaction.Transactional;
+import java.sql.SQLException;
 import java.util.Collection;
 import java.util.List;
 
@@ -32,14 +33,25 @@ public class QuestionRepository {
         }
     }
 
-    public void addQuestion(Question question, int subject_id){
-        //TODO add question with subjectId
-
+    public void addQuestion(Question question){
+        em.persist(question);
     }
 
     public void updateQuestion(Question question){
-        //TODO update question
-//        em.merge(question);
-//        em.flush();
+        em.merge(question);
+        em.flush();
+    }
+    public Question getQuestionByQuestionId(int question_id){
+        try {
+            StringBuilder sb = new StringBuilder();
+            sb.append("select q from Question q where question_id =:id");
+            String sql = sb.toString();
+            Query query = em.createQuery(sql, Question.class);
+            query.setParameter("id", question_id);
+            query.setMaxResults(3);
+            return (Question) query.getSingleResult();
+        }catch (NoResultException e){
+            return null;
+        }
     }
 }
