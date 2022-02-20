f697997a0259df92ce243db8c27b2389b7ba93dd
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-02-20 23:23:06 +0700

    code edit question

--- src/main/java/com/fpt/OnlineQuiz/controller/QuestionController.java
+++ src/main/java/com/fpt/OnlineQuiz/controller/QuestionController.java
@@ -1,6 +1,5 @@
 package com.fpt.OnlineQuiz.controller;
 
-import com.fpt.OnlineQuiz.dto.QuestionDTO;
 import com.fpt.OnlineQuiz.model.Answer;
 import com.fpt.OnlineQuiz.model.Question;
 import com.fpt.OnlineQuiz.model.Subject;
@@ -32,15 +31,12 @@ public class QuestionController {
     private SubjectService subjectService;
 
     @GetMapping(path = "/create")
-    String showCreateQuestionPage(ModelMap modelMap) {
-        modelMap.addAttribute("questionDTO", new QuestionDTO());
-
-//        modelMap.addAttribute("subject", )
+    String showCreateQuestionPage(ModelMap modelMap){
         return "create_question_page";
     }
 
     @PostMapping(path = "/addquestion")
-    public String processCreateQuestion(ModelMap modelMap, HttpServletRequest request) {
+    public String processCreateQuestion(ModelMap modelMap, HttpServletRequest request){
         Question q = new Question();
         q.setQuestion(request.getParameter("question").trim());
         int number = Integer.parseInt(request.getParameter("isAnswer"));
@@ -54,13 +50,13 @@ public class QuestionController {
         answerList.add(answer3);
         answerList.add(answer4);
         List<Answer> answers = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < 4; i++){
             Answer a = new Answer();
             a.setAnswer(answerList.get(i));
-            if (number == (i + 1)) {
+            if(number == (i + 1)){
                 q.setAnswer(a.getAnswer());
                 a.setCorrect(true);
-            } else {
+            }else{
                 a.setCorrect(false);
             }
             a.setQuestion(q);
@@ -78,25 +74,49 @@ public class QuestionController {
     }
 
     @GetMapping(path = "/edit")
-    String showEditQuestionPage(ModelMap modelMap) {
-        //TODO Code Edit Question
+    String showEditQuestionPage(ModelMap modelMap){
         Question q = questionService.getQuestionByQuestionId(1);
-        modelMap.addAttribute("questionDTO", new QuestionDTO());
         modelMap.addAttribute("question", q);
+        List<Answer> answers = answerService.getAnswers(1);
+        modelMap.addAttribute("answer", answers);
         return "edit_question_page";
     }
 
     @PostMapping(path = "/editquestion")
-    public String processEditQuestion(@ModelAttribute QuestionDTO questionDTO, ModelMap modelMap, @RequestParam int ques_id) {
+    public String processEditQuestion(ModelMap modelMap, HttpServletRequest request){
         Question q = new Question();
+        q.setQuestion(request.getParameter("question").trim());
+        int number = Integer.parseInt(request.getParameter("isAnswer"));
+        String answer1 = request.getParameter("answer1").trim();
+        String answer2 = request.getParameter("answer2").trim();
+        String answer3 = request.getParameter("answer3").trim();
+        String answer4 = request.getParameter("answer4").trim();
+        List<String> answerList = new ArrayList<>();
+        answerList.add(answer1);
+        answerList.add(answer2);
+        answerList.add(answer3);
+        answerList.add(answer4);
+        List<Answer> answers = new ArrayList<>();
+        for (int i = 0; i < 4; i++){
+            Answer a = new Answer();
+            a.setAnswer(answerList.get(i));
+            if(number == (i + 1)){
+                q.setAnswer(a.getAnswer());
+                a.setCorrect(true);
+            }else{
+                a.setCorrect(false);
+            }
+            a.setQuestion(q);
+            answers.add(a);
+        }
+        q.setAnswers(answers);
+        q.setExplain(request.getParameter("explain"));
         Subject subject = subjectService.getSubjectById(1);
-        q.setQuestion(questionDTO.getQuestion());
-//        q.setAnswer(questionDTO.getAnswer());
-        q.setExplain(questionDTO.getExplain());
         q.setSubject(subject);
-//        q.setId(ques_id);
-        questionService.updateQuestion(q);
-        modelMap.addAttribute("message", "Update successful!");
+        questionService.addQuestion(q);
+
+        answerService.addAnswers(answers);
+        modelMap.addAttribute("message", "Edit successful!");
         return "edit_question_page";
     }
 
@@ -104,8 +124,8 @@ public class QuestionController {
     String showListQuestionPage(@Param(value = "subId") int subId, Model model, HttpServletRequest request) {
         List<Question> questionList = questionService.getQuesitonBySubjectId(1);
         Subject subject = subjectService.getSubjectById(subId);
-        model.addAttribute("ques", questionList);
-        model.addAttribute("sub", subject);
+        model.addAttribute("ques",questionList);
+        model.addAttribute("sub",subject);
         return "question_list_page";
     }
 
