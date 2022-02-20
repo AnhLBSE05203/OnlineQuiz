e452c3b6e924936f35ae1620c36f213683ca4a58
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-02-20 22:48:23 +0700

    code addAnswer

---
+++ src/main/java/com/fpt/OnlineQuiz/service/AnswerService.java
@@ -0,0 +1,10 @@
+package com.fpt.OnlineQuiz.service;
+
+import com.fpt.OnlineQuiz.model.Answer;
+
+import java.util.List;
+
+
+public interface AnswerService {
+    void addAnswers(List<Answer> answers);
+}
