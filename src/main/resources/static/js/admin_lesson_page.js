cada63439728cf093fd9beca09377510e7bdb359
DaiKFD <97070839+NguyenTrongDai-HE153784-K14@users.noreply.github.com>
2022-02-21 16:20:48 +0700

    commitLesson4

--- src/main/resources/static/js/admin_lesson_page.js
+++ src/main/resources/static/js/admin_lesson_page.js
@@ -59,7 +59,7 @@ function recoverLesson(id){
 }
 function deleteLesson(id){
     window.location.replace("/admin/lesson/delete/" + id);
-}//
+}
 function showLessonEditModal(id) {
     var link = "/admin/lesson/" + id;
     var subject = "";
