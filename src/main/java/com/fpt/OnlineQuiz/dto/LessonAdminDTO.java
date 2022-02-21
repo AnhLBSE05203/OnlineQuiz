649e3515bbffb2a2b469fc4fe94fa2e05695b4a5
DaiKFD <97070839+NguyenTrongDai-HE153784-K14@users.noreply.github.com>
2022-02-21 20:53:56 +0700

    processLesson

---
+++ src/main/java/com/fpt/OnlineQuiz/dto/LessonAdminDTO.java
@@ -0,0 +1,13 @@
+package com.fpt.OnlineQuiz.dto;
+
+
+import com.fpt.OnlineQuiz.model.LessonType;
+
+public class LessonAdminDTO {
+    private int Lesid;
+    private String name;
+    private List<> lessonType;
+    private int totalCourse;
+    private String statusStr;
+    private int status;
+}
