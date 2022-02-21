ed43afff9d249773ab297bbaabd5adcecdee8cfe
DaiKFD <97070839+NguyenTrongDai-HE153784-K14@users.noreply.github.com>
2022-02-21 16:21:50 +0700

    commitLesson2

--- src/main/resources/static/js/admin_lesson_page.js
+++ src/main/resources/static/js/admin_lesson_page.js
@@ -59,7 +59,7 @@ function recoverLesson(id){
 }
 function deleteLesson(id){
     window.location.replace("/admin/lesson/delete/" + id);
-}//23
+}//234
 function showLessonEditModal(id) {
     var link = "/admin/lesson/" + id;
     var subject = "";
