8d1a64e2d0f240ed998d4e37254a6333499e9bab
DaiKFD <97070839+NguyenTrongDai-HE153784-K14@users.noreply.github.com>
2022-02-21 15:41:01 +0700

    processGiaoDien

---
+++ src/main/resources/static/js/admin_lesson_page.js
@@ -0,0 +1,100 @@
+$(document).ready(function() {
+    $('#LessonDatatable').DataTable({
+        "serverSide": true,
+        pageLength : 5,
+        ajax : {
+            url : '/admin/subject/getSubjectsByPage',
+            "type": "POST",
+            "dataType": "json",
+            "contentType": "application/json",
+            "data": function (d) {
+                return JSON.stringify(d);
+            }
+        },
+        columns : [ {
+            title : 'Id',
+            data : 'id'
+        }, {
+            title : 'Name',
+            data : 'name'
+        }, {
+            title : 'ImgSrc',
+            data : 'imgSrc',
+        }, {
+            title : 'Total Courses',
+            data : 'totalCourse',
+        }, {
+            title : 'Status',
+            data : 'statusStr',
+        }, {
+            title : 'Img',
+            data : 'imgSrc',
+            render: function(data, type, row, meta) {
+                var html = "";
+                if(data != "" && data != null){
+                    html += '<img src="' + data +'" class="subject-img" />';
+                }
+                return html;
+            }
+        }, {
+            title : 'Action',
+            data : 'status',
+            render: function(data, type, row, meta) {
+                var html = '<button type="button" class="btn btn-primary" onclick="showSubjectEditModal('+ row['id'] + ')">'
+                    + 'Edit</button>&nbsp';
+                if (data == 0) {
+                    html += '<button type="button" class="btn btn-primary" onclick="recoverSubject('+ row['id'] +')">'
+                        + 'Recover</button>&nbsp';
+                } else {
+                    html += '<button type="button" class="btn btn-primary" onclick="deleteSubject('+ row['id'] +')">'
+                        + 'Delete</button>&nbsp';
+                }
+                return html;
+            }
+        } ]
+    });
+});
+function recoverSubject(id){
+    window.location.replace("/admin/subject/recover/" + id);
+}
+function deleteSubject(id){
+    window.location.replace("/admin/subject/delete/" + id);
+}
+function showSubjectEditModal(id) {
+    var link = "/admin/subject/" + id;
+    var subject = "";
+    $.ajax({
+        url: link,
+        type:"get",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data){
+            subject = data;
+            if(subject != ""){
+                $("#editSubjectId").val(subject.id);
+                $("#editSubjectName").val(subject.name);
+                $("#editSubjectTotalCourse").val(subject.totalCourse);
+                $("#editSubjectImg").attr("src", subject.imgSrc);
+                $("#editSubjectStatus").val(subject.status).change();
+
+            }
+        }
+    });
+    $('#subjectEditModal').modal('show');
+};
+$('#subjectEditModal').on('hidden.bs.modal', function () {
+    alert('close');
+});
+$('#subjectEditModal').on('shown.bs.modal', function (e) {
+    // do something...
+});
+
+function showLessonAddModal(id) {
+    $('#subjectAddModal').modal('show');
+};
+$('#subjectAddModal').on('hidden.bs.modal', function () {
+    alert('close');
+});
+$('#subjectAddModal').on('shown.bs.modal', function (e) {
+    // do something...
+});
