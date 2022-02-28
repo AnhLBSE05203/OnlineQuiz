e400b5956652a7f4738b6cfc0362344616c38444
lazycatgod <39523975+lazycatgod@users.noreply.github.com>
2022-03-01 00:24:53 +0700

    add account admin dto

---
+++ src/main/java/com/fpt/OnlineQuiz/dto/AccountAdminDTO.java
@@ -0,0 +1,22 @@
+package com.fpt.OnlineQuiz.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountAdminDTO {
+    private int id;
+
+    private String fullName;
+
+    private int gender;
+
+    private String email;
+
+    private String phone;
+}
