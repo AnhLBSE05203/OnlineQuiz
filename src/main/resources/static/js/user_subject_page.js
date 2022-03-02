d9d547f081c0d5d9777e001c756b70fb02e03393
quangvu2k <quangvhe140807@fpt.edu.vn>
2022-03-02 23:27:48 +0700

    fix user_subject_page.js

--- src/main/resources/static/js/user_subject_page.js
+++ src/main/resources/static/js/user_subject_page.js
@@ -1,5 +1,65 @@
-$(document).ready(function (){
-    function detailSubjectModal(){
-        $('#myModal').modal('show');
-    }
+$(document).ready(function () {
+
 })
+
+function detailSubjectModal(id){
+    var link = "/subject/detail/" + id;
+    console.log(id);
+    var subject = "";
+    $.ajax({
+        url: link,
+        type: "get",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            subject = data;
+            if (subject != null) {
+                console.log("success");
+                console.log(subject.subjectInfo);
+                $("#subjectInfo").text(subject.subjectInfo);
+                $("#subjectLearn").text(subject.learnAfter);
+            }else{
+                console.log("null")
+            }
+        }
+    });
+    $('#myModal').modal('show');
+}
+
+// var link = "/subject/detail/" + id;
+// console.log(id);
+// var subject = "";
+// $.ajax({
+//     url:link,
+//     type:"get",
+//     contentType:"application/json; charset=utf-8",
+//     dataType:"json",
+//     success:function (data){
+//         subject = data;
+//         if(subject != null){
+//             $("#subjectInfo").val(subject.subjectinfo);
+//         }
+//     }
+// });
+// function showSubjectEditModal(id) {
+//     var link = "/admin/subject/" + id;
+//     var subject = "";
+//     $.ajax({
+//         url: link,
+//         type:"get",
+//         contentType: "application/json; charset=utf-8",
+//         dataType: "json",
+//         success: function (data){
+//             subject = data;
+//             if(subject != ""){
+//                 $("#editSubjectId").val(subject.id);
+//                 $("#editSubjectName").val(subject.name);
+//                 $("#editSubjectTotalCourse").val(subject.totalCourse);
+//                 $("#editSubjectImg").attr("src", subject.imgSrc);
+//                 $("#editSubjectStatus").val(subject.status).change();
+//
+//             }
+//         }
+//     });
+//     $('#subjectEditModal').modal('show');
+// };
