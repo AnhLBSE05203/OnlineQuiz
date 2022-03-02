905d11491c7399ca9d4b628d78f7d3dc56b98cdf
DaiKFD <97070839+NguyenTrongDai-HE153784-K14@users.noreply.github.com>
2022-03-02 23:04:47 +0700

    getHocPhan

--- src/main/java/com/fpt/OnlineQuiz/service/implement/QuizPackageImpl.java
+++ src/main/java/com/fpt/OnlineQuiz/service/implement/QuizPackageImpl.java
@@ -1,15 +1,21 @@
 package com.fpt.OnlineQuiz.service.implement;
 
+import com.fpt.OnlineQuiz.dao.CRUDRepository.CRUDQuizPackage;
 import com.fpt.OnlineQuiz.model.QuizPackage;
 import com.fpt.OnlineQuiz.service.QuizPackageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class QuizPackageImpl implements QuizPackageService {
+
+    @Autowired
+    CRUDQuizPackage quizPackage;
+
     @Override
     public List<QuizPackage> finByAccountId(int id) {
-        return finByAccountId(id);
+        return quizPackage.findByAccountId(id);
     }
 }
