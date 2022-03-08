1d65b4729154e89dbcdfacd9e411bbec29a60a70
Bao Anh Luu <anhlbse05203@fpt.edu.vn>
2022-03-08 08:05:45 +0700

    upload file controller update 1

--- src/main/java/com/fpt/OnlineQuiz/controller/UploadImageController.java
+++ src/main/java/com/fpt/OnlineQuiz/controller/UploadImageController.java
@@ -1,17 +1,14 @@
 package com.fpt.OnlineQuiz.controller;
 
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.fpt.OnlineQuiz.service.UploadImageService;
-import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import com.amazonaws.services.s3.AmazonS3;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -28,8 +25,26 @@ public class UploadImageController {
     @Autowired
     private UploadImageService uploadImageService;
 
+    //todo:
+    // 1. process edit/add for other fields (id, name, etc.) & create proxy Image object
+    //    on other controller. e.g: admin/subject/edit or admin/subject/add
+    // 2. provide more @param on previous controller (request.addAttribute("...","..."))
+    //       a. return link to redirect after file upload. e.g: "redirect:/admin/subject"
+    //       b. created Image Id
+    // 3. upload file here, get Image from DB through Id, add link to Image's imgSrc
+
+    /**
+     * Upload file (image) to AWS & update image source into DB after processing add/edit on other controller
+     * Then redirect User to provided return link after uploading file
+     *
+     * @param file       file chosen to be uploaded
+     * @param returnLink link to be redirected to after uploading, provided on previous controller
+     * @param imgId      Image object's ID to process saving imgSrc to DB, provided on previous controller
+     * @return
+     * @throws IOException
+     */
     @PostMapping("/imageMultipartFile")
-    public Map<String, String> uploadMultipartFile(@RequestParam("file") MultipartFile[] file) throws IOException {
+    public String uploadMultipartFile(@RequestParam("file") MultipartFile[] file, @RequestParam("returnLink") String returnLink, @RequestParam("imgId") int imgId) throws IOException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmssSSS");
         Map<String, String> values = new HashMap<String, String>();
         String date = simpleDateFormat.format(new Date());
@@ -45,25 +60,26 @@ public class UploadImageController {
                     this.s3client.putObject(new PutObjectRequest("auctionimage", "image/" + fileName, fileOut)
                             .withCannedAcl(CannedAccessControlList.PublicRead));
                     values.put("imageUrl", "https://auctionimage.s3.ap-southeast-1.amazonaws.com/image/" + fileName);
-                    return values;
+                    return returnLink;
                 } catch (Exception e) {
                     values.put("imageUrl", "");
-                    return values;
+                    return returnLink;
                 }
             }
         }
         values.put("imageUrl", "");
-        return values;
+        return returnLink;
     }
 
     @PostMapping("/imageMultipartFiles")
-    public ResponseEntity<?> uploadMultipartFiles(@RequestParam("file") MultipartFile[] file) throws IOException {
+    public String uploadMultipartFiles(@RequestParam("file") MultipartFile[] file, @RequestParam("returnLink") String returnLink, @RequestParam("imgId") int imgId) throws IOException {
         List<String> listImage = new ArrayList<>();
         if (file != null && file.length != 0) {
             listImage = this.uploadImageService.saveFileToS3(file);
         } else {
             listImage.add("");
         }
-        return ResponseEntity.ok(listImage);
+        //return ResponseEntity.ok(listImage);
+        return returnLink;
     }
 }
