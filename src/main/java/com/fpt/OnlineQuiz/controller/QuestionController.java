546eca96e527ac7b63c55c233cd98733c652b771
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-03-12 16:17:40 +0700

    update QuestionController

--- src/main/java/com/fpt/OnlineQuiz/controller/QuestionController.java
+++ src/main/java/com/fpt/OnlineQuiz/controller/QuestionController.java
@@ -4,9 +4,11 @@ import com.fpt.OnlineQuiz.dto.QuestionAdminDTO;
 import com.fpt.OnlineQuiz.dto.paging.Page;
 import com.fpt.OnlineQuiz.dto.paging.PagingRequest;
 import com.fpt.OnlineQuiz.model.Answer;
+import com.fpt.OnlineQuiz.model.Lesson;
 import com.fpt.OnlineQuiz.model.Question;
 import com.fpt.OnlineQuiz.model.Subject;
 import com.fpt.OnlineQuiz.service.AnswerService;
+import com.fpt.OnlineQuiz.service.LessonService;
 import com.fpt.OnlineQuiz.service.QuestionService;
 import com.fpt.OnlineQuiz.service.SubjectService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,14 +32,15 @@ public class QuestionController {
     private AnswerService answerService;
 
     @Autowired
-    private SubjectService subjectService;
+    private LessonService lessonService;
 
     @GetMapping(path = "/create")
     String showCreateQuestionPage(ModelMap modelMap, HttpServletRequest request) {
         //todo - fix hardcode subjectId
+
 //        modelMap.addAttribute("questionDTO", new QuestionDTO());
 //        String subjectIdStr = request.getParameter("subjectId");
-        int subjectId = 1;
+        int lessonId = Integer.parseInt(request.getParameter("lessonId"));
 //        if (subjectIdStr != null && subjectIdStr.trim() != "") {
 //            try{
 //                subjectId = Integer.parseInt(subjectIdStr);
@@ -46,15 +49,15 @@ public class QuestionController {
 //                return "redirect:/home";
 //            }
 //        }
-        modelMap.addAttribute("subjectId", subjectId);
+        modelMap.addAttribute("lessonId", lessonId);
         return "create_question_page";
     }
 
     @PostMapping(path = "/addquestion")
     public String processCreateQuestion(ModelMap modelMap, HttpServletRequest request) {
-        int subjectId = Integer.parseInt(request.getParameter("subjectId"));
-        Subject subject = subjectService.getSubjectById(subjectId);
-        if (subject == null) {
+        int lessonId = Integer.parseInt(request.getParameter("lessonId"));
+        Lesson lesson = lessonService.getLessonByLessonId(lessonId);
+        if (lesson == null) {
             //redirect to wherever if subject doesn't exist
             //use RedirectAttributes if wanting to pass an error message, maybe
             return "redirect:/home";
@@ -87,7 +90,7 @@ public class QuestionController {
         q.setAnswers(answers);
         q.setExplain(request.getParameter("explain"));
 
-        q.setSubject(subject);
+        q.setLesson(lesson);
         questionService.addQuestion(q);
         answerService.addAnswers(answers);
         modelMap.addAttribute("message", "Add successful!");
@@ -96,22 +99,20 @@ public class QuestionController {
 
     @GetMapping(path = "/edit")
     String showEditQuestionPage(ModelMap modelMap, HttpServletRequest request) {
-        String questionId = request.getParameter("questionId");
-
+        String questionId = request.getParameter("questionId").trim();
         Question q = questionService.getQuestionByQuestionId(Integer.parseInt(questionId));
         // get subjectId through question, not param
         // then since subject is a proxy object (only contains Id), get subject through the id
-        int subjectId = q.getSubject().getId();
         modelMap.addAttribute("question", q);
-        modelMap.addAttribute("subjectId", subjectId);
         List<Answer> answers = answerService.getAnswers(Integer.parseInt(questionId));
-        modelMap.addAttribute("answer", answers);
+        modelMap.addAttribute("answers", answers);
         return "edit_question_page";
     }
 
     @PostMapping(path = "/editquestion")
     public String processEditQuestion(ModelMap modelMap, HttpServletRequest request) {
         //todo - edit not add
+        int lessonId = Integer.parseInt(request.getParameter("lessonId"));
         Question q = new Question();
         //int questionId = Integer.parseInt(request.getParameter("questionId").trim());
         //Question q = questionService.getQuestionByQuestionId(questionId);
@@ -119,7 +120,7 @@ public class QuestionController {
         q.setId(Integer.parseInt(request.getParameter("questionId")));
         int number = Integer.parseInt(request.getParameter("isAnswer"));
         //todo - either delete all Answers related to the Question then add new
-//        answerService.deleteAnswerByQuestionId(questionId);
+//        answerService.deleteAnswerByQuestionId(q.getId());
         //todo - or edit the answers themselves
         String answer1 = request.getParameter("answer1").trim();
         String answer2 = request.getParameter("answer2").trim();
@@ -145,28 +146,24 @@ public class QuestionController {
             answers.add(a);
         }
         q.setAnswers(answers);
-        q.setExplain(request.getParameter("explain"));
+        q.setExplain(request.getParameter("explain").trim());
         //todo - get subjectId through question instead
         // what if the Subject in @param is not the one that owns the Question in @param?
-        Subject subject = subjectService.getSubjectById(Integer.parseInt(request.getParameter("subjectId")));
-        q.setSubject(subject);
-        questionService.updateQuestion(q);
-
+        Lesson lesson = lessonService.getLessonByLessonId(lessonId);
+        q.setLesson(lesson);
         answerService.updateAnswers(answers);
+        questionService.updateQuestion(q);
         modelMap.addAttribute("message", "Edit successful!");
-        List<Question> questionList = questionService.getQuestionBySubjectId(1);
+        List<Question> questionList = questionService.getQuestionByLessonId(lesson.getId());
         modelMap.addAttribute("question_list", questionList);
-        modelMap.addAttribute("sub", subject);
         return "admin_list_question_page";
     }
 
     @GetMapping(path = "/list")
     String showQuestionListPage(ModelMap model) {
         //todo - fix hardcode subjectId
-        List<Question> questionList = questionService.getQuestionBySubjectId(1);
-        Subject subject = subjectService.getSubjectById(1);
+        List<Question> questionList = questionService.getQuestionByLessonId(1);
         model.addAttribute("question_list", questionList);
-        model.addAttribute("sub", subject);
         return "admin_list_question_page";
     }
 
@@ -186,18 +183,20 @@ public class QuestionController {
 
     @GetMapping(path = "/delete")
     public String deleteQuestion(ModelMap modelMap, HttpServletRequest request) {
-        int questionId = Integer.parseInt(request.getParameter("questionId"));
-        //todo - get subjectId through Question
-        // what if the Subject in @param is not the one that owns the Question in @param?
-        int subjectId = Integer.parseInt(request.getParameter("subjectId"));
-        answerService.deleteAnswerByQuestionId(questionId);
+        try{
+            int questionId = Integer.parseInt(request.getParameter("questionId"));
+            //todo - get subjectId through Question
+            // what if the Subject in @param is not the one that owns the Question in @param?
+            Question question = questionService.getQuestionByQuestionId(questionId);
+            answerService.deleteAnswerByQuestionId(questionId);
 
-        questionService.deleteQuestion(questionId);
-        List<Question> questionList = questionService.getQuestionBySubjectId(subjectId);
-        Subject subject = subjectService.getSubjectById(subjectId);
-        modelMap.addAttribute("question_list", questionList);
-        modelMap.addAttribute("sub", subject);
-        modelMap.addAttribute("message", "Delete successful!");
+            questionService.deleteQuestion(questionId);
+            List<Question> questionList = questionService.getQuestionByLessonId(question.getLesson().getId());
+            modelMap.addAttribute("question_list", questionList);
+        }catch (NullPointerException exception){
+            exception.printStackTrace();
+        }
+//        modelMap.addAttribute("message", "Delete successful!");
         return "admin_list_question_page";
     }
 }
