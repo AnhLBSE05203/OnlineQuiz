2ba2d56fecdbb354f97efc5aff0decf40931aeed
Bao Anh Luu <anhlbse05203@fpt.edu.vn>
2022-03-14 15:44:31 +0700

    refractor

--- src/main/java/com/fpt/OnlineQuiz/dao/CRUDRepository/CRUDQuestionRepository.java
+++ src/main/java/com/fpt/OnlineQuiz/dao/CRUDRepository/CRUDQuestionRepository.java
@@ -1,6 +1,5 @@
 package com.fpt.OnlineQuiz.dao.CRUDRepository;
 
-import com.fpt.OnlineQuiz.model.Lesson;
 import com.fpt.OnlineQuiz.model.Question;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CRUSQuesionRepository extends CrudRepository<Question,Integer> {
+public interface CRUDQuestionRepository extends CrudRepository<Question, Integer> {
 
     @Query("select q from Question q where q.lesson.id = ?1")
     List<Question> findBySubId(int subId);
