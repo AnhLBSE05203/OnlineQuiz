7f537925bf81f61789d2f04975a8fc4448cca2d3
quangvu2k <quangvhe140807@fpt.edu.vn>
2022-03-20 18:06:49 +0700

    create quiz_handle_1.js

---
+++ src/main/resources/static/js/quiz_handle_1.js
@@ -0,0 +1,19 @@
+function getQandA() {
+    var qid = document.getElementById("question").value;
+    // var answer = $(".option span").val();
+    console.log(qid);
+    // $.ajax({
+    //     type: "POST",
+    //     url: "/quizhandelecontroller/quizpost",
+    //     data: {
+    //         qid: $(".que_text span").val(), // < note use of 'this' here
+    //         answer: $(".option span").val()
+    //     },
+    //     success: function(result) {
+    //         alert('ok');
+    //     },
+    //     error: function(result) {
+    //         alert('error');
+    //     }
+    // });
+}
