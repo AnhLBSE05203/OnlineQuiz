b9c7f0b8a07c9efb98114990ae8cb20110a3e402
DaiKFD <97070839+NguyenTrongDai-HE153784-K14@users.noreply.github.com>
2022-03-21 06:12:59 +0700

    addSubjectRequied

--- src/main/java/com/fpt/OnlineQuiz/controller/LessonController.java
+++ src/main/java/com/fpt/OnlineQuiz/controller/LessonController.java
@@ -1,9 +1,11 @@
 package com.fpt.OnlineQuiz.controller;
 
+import com.fpt.OnlineQuiz.dto.CourseFeaturedDTO;
+import com.fpt.OnlineQuiz.dto.ExpertFeaturedDTO;
 import com.fpt.OnlineQuiz.model.Lesson;
 import com.fpt.OnlineQuiz.model.Subject;
-import com.fpt.OnlineQuiz.service.LessonService;
-import com.fpt.OnlineQuiz.service.SubjectService;
+import com.fpt.OnlineQuiz.service.*;
+import com.fpt.OnlineQuiz.utils.Constants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
@@ -23,9 +25,27 @@ public class LessonController {
 
     @Autowired
     private SubjectService subjectService;
+    @Autowired
+    QuizHistoryService quizHistoryService;
+    @Autowired
+    QuizHistoryAccountAddServices accountAddServices;
+    @Autowired
+    QuestionService questionService;
+
+    @Autowired
+    private CourseService courseService;
+    @Autowired
+    private ExpertService expertService;
 
     @GetMapping("/list")
     public String showLessonListPage(@Param(value = "subId") int subId, Model model, HttpServletRequest request) {
+        List<CourseFeaturedDTO> courseFeatured = courseService.getFeaturedCourses(Constants.HOME_PAGE_COURSE_NUMBER);
+        model.addAttribute(Constants.HOME_PAGE_ATTRIBUTE_COURSE_FEATURED, courseFeatured);
+        List<ExpertFeaturedDTO> expertFeatured = expertService.getFeaturedExperts(Constants.HOME_PAGE_EXPERT_NUMBER);
+        model.addAttribute(Constants.HOME_PAGE_ATTRIBUTE_EXPERT_FEATURED, expertFeatured);
+        List<Subject> subjectFeatured = subjectService.getFeaturedSubjects(Constants.HOME_PAGE_SUBJECT_NUMBER);
+        model.addAttribute(Constants.HOME_PAGE_ATTRIBUTE_SUBJECT_FEATURED, subjectFeatured);
+
         List<Lesson> list = lessonService.getAllLesson(subId);
         Optional<Subject> subject = subjectService.getSubject(subId);
 
