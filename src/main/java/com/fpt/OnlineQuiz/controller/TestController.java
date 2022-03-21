82c3d663395d8762bf6b6d1c819901217a9b0603
tuannvhe141379 <tuannvhe141379@fpt.edu.vn>
2022-03-22 01:38:13 +0700

    update get number of question

--- src/main/java/com/fpt/OnlineQuiz/controller/TestController.java
+++ src/main/java/com/fpt/OnlineQuiz/controller/TestController.java
@@ -1,18 +1,27 @@
 package com.fpt.OnlineQuiz.controller;
 
+import com.fpt.OnlineQuiz.model.Question;
+import com.fpt.OnlineQuiz.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 @RequestMapping("/test")
 public class TestController {
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping(path = "")
     public void test(HttpServletRequest request){
         int lesson_id = Integer.parseInt(request.getParameter("lessonId"));
         int number = Integer.parseInt(request.getParameter("number"));
-        System.out.println("lessonId = " + lesson_id + " number: " + number);
+        List<Question> questionList = questionService.getNumberOfQuestionByLessonId(lesson_id, number);
+        System.out.println(questionList);
     }
 }
